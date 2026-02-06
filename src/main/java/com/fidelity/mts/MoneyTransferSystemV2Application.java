b05751d0e782d72852ebefdb36b0d693336e8a37
Aashifa <aashifa@student.tce.edu>
2026-02-06 08:52:43 +0000

    Adding files for money transfer system

---
+++ src/main/java/com/fidelity/mts/MoneyTransferSystemV2Application.java
@@ -0,0 +1,13 @@
+package com.fidelity.mts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferSystemV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTransferSystemV2Application.class, args);
+	}
+
+}
